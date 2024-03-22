8e0cdd99cae33cd80924d9b5192b80696d7590d7
mhnrhmni <mahanrahmani777@gmail.com>
2024-03-22 10:32:48 +0330

    M

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="fakefarsi",
+    version="0.0.1",
+    author="Mahan Rahmani",
+    author_email="mahanrahmani777@gmail.com",
+    description="A package for generate a fake iranian person information",
+    long_description=readme,
+    url="https://github.com/mhnrhmni/FakeFarsi/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Mit License",
+        "Operating System :: OS Independent",
+    ],
+)
